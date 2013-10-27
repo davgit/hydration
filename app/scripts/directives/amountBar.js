d72f9c2fecff025c9d6e0e937e0ca8e68a77d685
owenmead <owenmead@gmail.com>
2013-10-27 14:57:00 -0700

    Rect growing starting to take shape

--- app/scripts/directives/amountBar.js
+++ app/scripts/directives/amountBar.js
@@ -5,13 +5,36 @@ angular.module('hydrationApp')
     return {
       restrict: 'A',
       link: function postLink(scope, element, attrs) {
+        var height = element[0].offsetHeight;
+        var width = element[0].offsetWidth;
 
-       var svg = d3.select(element[0]);
+        var svg = d3.select(element[0]);
 
-        svg.append("rect")
-          .attr("class", "fillBar")
-          .attr("height", 200)
-          .attr("width", 300)
+        var data = [20];
+
+        var scale = d3.scale.linear()
+          .domain([0, 100])
+          .range([height, 0]);
+
+        svg.selectAll(".amountBar")
+          .data(data)
+        .enter().append("rect")
+          .attr("class", "amountBar")
+          .attr("x", 40)
+          .attr("width", width-40*2)
+          .attr("y", function(d) { return scale(d); })
+          .attr("height", function(d) { return height - scale(d); });
+
+
+        scope.$watch("amount", function() {
+          svg.selectAll(".amountBar")
+            .data([scope.amount])
+            .transition()
+            .ease("quad-in-out")
+            .duration(700)
+            .attr("y", function(d) { return scale(d); })
+            .attr("height", function(d) { return height - scale(d); });
+        });
       }
     };
   });
