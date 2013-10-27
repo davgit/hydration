b4130d715edf775438caba63afd6ec97e9606bb9
owenmead <owenmead@gmail.com>
2013-10-27 11:34:16 -0700

    Add in the left and right axis

--- app/scripts/directives/mainScreen.js
+++ app/scripts/directives/mainScreen.js
@@ -3,10 +3,50 @@
 angular.module('hydrationApp')
   .directive('mainScreen', function () {
     return {
-      template: '<div></div>',
+      template: '<div class="mainScreen"></div>',
       restrict: 'A',
+      replace: true,
       link: function postLink(scope, element, attrs) {
-        element.text('d3 graphic will go here');
+        var height = element[0].offsetHeight;
+        var width = element[0].offsetWidth;
+
+        var svg = d3.selectAll("div.mainScreen")
+          .append("svg");
+
+        var left_scale = d3.scale.linear()
+          .domain([0, 100])
+          .range([height, 0]);
+
+        var leftAxis = d3.svg.axis()
+          .scale(left_scale)
+          .tickPadding(5)
+          .tickValues(left_scale.ticks().slice(1, -1))
+          .outerTickSize(0)
+          .orient("right");
+
+        var left_bar = svg.append("g")
+          .attr("class", "left axis")
+          .call(leftAxis);
+
+        var start_date = new Date(2013, 9, 27, 7);
+        var end_date = new Date(2013, 9, 27, 11);
+
+        var right_scale = d3.time.scale()
+          .domain([start_date, end_date])
+          .range([height, 0])
+
+        var rightAxis = d3.svg.axis()
+          .scale(right_scale)
+          .tickPadding(5)
+          .tickValues(right_scale.ticks().slice(1, -1))
+          .outerTickSize(0)
+          .orient("left")
+
+        var right_bar = svg.append("g")
+          .attr("transform", "translate("+width+", 0)")
+          .attr("class", "right axis")
+          .call(rightAxis)
+
       }
     };
   });
