2d95a72baf8a2d8d4b923ff98b5475e1ad04a9da
owenmead <owenmead@gmail.com>
2013-11-05 21:09:58 -0800

    Move water amount changing logic to controller, where it should be

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,5 +2,15 @@
 
 angular.module('hydrationApp')
   .controller('MainCtrl', function ($scope) {
-    // awesome controller code here
+    $scope.amount = 0;
+
+
+    $scope.large_glass = function() {
+      $scope.amount = ($scope.amount + 10)%110;
+    };
+
+    $scope.small_glass = function() {
+      $scope.amount = Math.abs(($scope.amount - 10)%110);
+    };
+
   });
