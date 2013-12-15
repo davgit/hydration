adbabd6339cdd9b0e25403453e8b767c35b5bc5e
owenmead <owenmead@gmail.com>
2013-12-15 14:23:34 -0800

    Building out the intro page a bit

--- app/scripts/controllers/intro.js
+++ app/scripts/controllers/intro.js
@@ -2,5 +2,15 @@
 
 angular.module('hydrationApp')
   .controller('IntroCtrl', function ($scope) {
-    // intro controller stuff goes here
+	$scope.model = {
+		weight: 0
+	};
+
+	$scope.$watch('weight_input', function(new_val) {
+		if (new_val === undefined || new_val == NaN) {
+			return;
+		} else {
+			$scope.model.weight = new_val;
+		}
+	});
   });
