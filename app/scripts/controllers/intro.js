7eb9a55975fbaf6ec14d6178b82b86d8fda8923a
owenmead <owenmead@gmail.com>
2013-12-15 15:31:08 -0800

    Move to a next/previous style

--- app/scripts/controllers/intro.js
+++ app/scripts/controllers/intro.js
@@ -2,15 +2,11 @@
 
 angular.module('hydrationApp')
   .controller('IntroCtrl', function ($scope) {
+
 	$scope.model = {
-		weight: 0
+		show_question: 'weight',
+		weight: 0,
+		units: null
 	};
 
-	$scope.$watch('weight_input', function(new_val) {
-		if (new_val === undefined || new_val == NaN) {
-			return;
-		} else {
-			$scope.model.weight = new_val;
-		}
-	});
   });
