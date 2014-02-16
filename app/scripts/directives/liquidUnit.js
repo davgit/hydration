8a3faefc13be81ca5ef84f2aefce8d08d93cc935
owenmead <owenmead@gmail.com>
2014-02-15 21:59:56 -0800

    Remove select style as we will use segment button instead

--- app/scripts/directives/liquidUnit.js
+++ app/scripts/directives/liquidUnit.js
@@ -3,8 +3,8 @@
 angular.module('hydrationApp')
   .directive('liquidUnit', function ($rootScope) {
     return {
-      template: '<button ng-click="set_unit(\'ml\')" ng-class="{\'ml\': \'selected\'}[model.liquid_units]" class="multi_choice">250 ml</button>'+
-				'<button ng-click="set_unit(\'oz\')" ng-class="{\'oz\': \'selected\'}[model.liquid_units]" class="multi_choice">8 oz</button>',
+      template: '<a ng-click="set_unit(\'ml\')" ng-class="{\'ml\': \'selected\'}[model.liquid_units]" class="multi_choice button">250 ml</a>'+
+				'<a ng-click="set_unit(\'oz\')" ng-class="{\'oz\': \'selected\'}[model.liquid_units]" class="multi_choice button">8 oz</a>',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
         scope.set_unit = function(unit) {
