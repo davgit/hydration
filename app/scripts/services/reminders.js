ecd1711dbc0156f173effdbb4d2ac093491c6c53
owenmead <owenmead@gmail.com>
2014-03-30 21:48:36 -0700

    Hook in to notification plugin for reminder

--- app/scripts/services/reminders.js
+++ app/scripts/services/reminders.js
@@ -1,11 +1,19 @@
 'use strict';
 
+// Using : https://github.com/katzer/cordova-plugin-local-notifications
+
 angular.module('hydrationApp')
   .service('reminders', function Reminders() {
     return {
       set_reminder: function(reminder_date) {
-        console.log("Set Reminder");
-        console.log(reminder_date);
+        // Kill all previous notifications, then add our new one
+        window.plugin.notification.local.cancelAll();
+        window.plugin.notification.local.add({
+            date:       reminder_date,
+            message:    'Friendly reminder to drink some water',
+            title:      'Drink Water',
+            autoCancel: true
+        });
       }
     }
   });
